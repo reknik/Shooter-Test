d35b4c1ef0f201c0f295db6710c19589bf5e7071
reknik <68144310+reknik@users.noreply.github.com>
2020-12-08 18:36:39 +0200

    Update player.lua

--- load/player.lua
+++ load/player.lua
@@ -1,33 +1,59 @@
 jucator={}
-jucator.x=0
-jucator.y=0
-jucator.inaltime = 51
-jucator.latime=57
+jucator.x=28
+jucator.y=25
+jucator.vx=0
+jucator.vy=0
+jucator.inaltime = 50
+jucator.latime=56
 jucator.sprite = sprites.jucator
 world:add(jucator,jucator.x,jucator.y,jucator.latime,jucator.inaltime)
 
-bullets={}
-speed=15
-rate=0.2
-rate_time=0
-
-function bullets.spawn(x,y,dx,dy,dir)
-    table.insert(bullets,{x=x,y=y,dx=dx,dy=dy,dir=dir})
-end
-
-function bullets.draw()
-  for i,v in ipairs(bullets) do
-    love.graphics.draw(sprites.bullet, v.x, v.y, v.dir,1,1,20,10)
+local playerFilter = function(item, other)
+  if     other.isBullet   then return 'cross'
+  elseif other.isEnemy   then return 'slide'
+  elseif other.isExit   then return 'touch'
+  elseif other.isSpring then return 'bounce'
   end
+  -- else return nil
 end
 
-function bullets.update(dt)
-  for i,v in ipairs(bullets) do
-    v.x=v.x+v.dx*speed*dt
-    v.y=v.y+v.dy*speed*dt
-    if(v.x<-10 or v.x>650 or v.y<10 or v.y>650) then
-      table.remove(bullets,i)
+function jucator:move(jucator, dt)
+  local goalX, goalY = jucator.x + jucator.vx * dt*150, jucator.y + jucator.vy * dt*150
+  local actualX, actualY, cols = world:move(jucator, goalX, goalY, playerFilter)
+  jucator.x, jucator.y = actualX, actualY
+  world:update(jucator, jucator.x, jucator.y)
+end
 
-    end
+
+function jucator:update(dt)
+  jucator.vx=0
+  jucator.vy=0
+
+  jucator.miscare=false
+  if love.keyboard.isDown("up") or love.keyboard.isDown("w") then
+    jucator.vy=-1
+    jucator.miscare=true
   end
+
+  if love.keyboard.isDown("down") or love.keyboard.isDown("s") then
+
+    jucator.vy=1
+    jucator.miscare=true
+  end
+
+  if love.keyboard.isDown("left") or love.keyboard.isDown("a") then
+
+    jucator.vx=-1
+    jucator.miscare=true
+
+  end
+  if love.keyboard.isDown("right") or love.keyboard.isDown("d") then
+
+    jucator.vx=1
+    jucator.miscare=true
+
+  end
+  jucator:move(jucator,dt)
+
 end
+
